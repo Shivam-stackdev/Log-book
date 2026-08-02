0eaaac0433384f446c0cfc5ec317861f122d9175
Shivam-stackdev <shivamnawada256@gmail.com>
2026-08-02 18:20:52 +0530

    Add import for dart:ui in dashboard_screen.dart

--- lib/features/inventory/presentation/screens/dashboard_screen.dart
+++ lib/features/inventory/presentation/screens/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'dart:ui';
 import 'package:army_mess_inventory/features/inventory/presentation/widgets/glass_widgets.dart';
 
 class DashboardScreen extends ConsumerWidget {
