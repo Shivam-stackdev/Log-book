8430ede286021e18f1b60e3b18c776784625c292
Shivam-stackdev <shivamnawada256@gmail.com>
2026-08-02 18:07:02 +0530

    Fix import statement in reports_screen.dart

--- lib/features/inventory/presentation/screens/reports_screen.dart
+++ lib/features/inventory/presentation/screens/reports_screen.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
+oppoimport 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:army_mess_inventory/features/inventory/presentation/providers/inventory_providers.dart';
 import 'package:army_mess_inventory/features/reports/data/pdf_report_service.dart';
 import 'package:army_mess_inventory/features/inventory/presentation/widgets/glass_widgets.dart';
-
+import 'package:go_router/go_router.dart';
 class ReportsScreen extends ConsumerWidget {
   const ReportsScreen({super.key});
 
